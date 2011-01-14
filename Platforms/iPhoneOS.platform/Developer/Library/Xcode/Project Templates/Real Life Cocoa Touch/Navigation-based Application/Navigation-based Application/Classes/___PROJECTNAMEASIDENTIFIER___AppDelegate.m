35f71309a4674e6e1ad3aea9fe210ee30d7f9eeb
Matt Nunogawa <hidden@example.com>
2011-01-13 20:11:22 -0800

    update nav-based app delegate

--- Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Real Life Cocoa Touch/Navigation-based Application/Navigation-based Application/Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.m
+++ Platforms/iPhoneOS.platform/Developer/Library/Xcode/Project Templates/Real Life Cocoa Touch/Navigation-based Application/Navigation-based Application/Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.m
@@ -31,24 +31,70 @@
 
 /*********************************************************************/
 #pragma mark -
+#pragma mark ** Utilities **
+
+- (void)saveState;
+{
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+/*********************************************************************/
+#pragma mark -
 #pragma mark ** Application Lifecycle **
 
-- (void)applicationDidFinishLaunching:(UIApplication *)application;
-{
-    // Override point for customization after app launch    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{    
+    // Override point for customization after app launch. 
     
     [self.window addSubview:[self.navigationController view]];
     [self.window makeKeyAndVisible];
+	return YES;
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application;
+- (void)applicationWillResignActive:(UIApplication *)application
 {
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    // Sent when the application is about to move from active to inactive state. 
+    // This can occur for certain types of temporary interruptions (such as an
+    // incoming phone call or SMS message) or when the user quits the 
+    // application and it begins the transition to the background state.
+    //
+    // Use this method to pause ongoing tasks, disable timers, and throttle 
+    // down OpenGL ES frame rates. Games should use this method to pause the game.
+
+    [self saveState];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the 
+    // application was inactive. If the application was previously in the 
+    // background, optionally refresh the user interface.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    // Sent when the application is put in the background
+    // This typically means that a user has switched away from this
+    // app to bring another app to the foreground.
+    // This method should complete within approximately 5 seconds
+    // or the system may force the app into suspended mode or 
+    // simply terminate the app
+    
+    // Unless you are running background tasks, 
+    // applicationWillResignActive: is the typical method where
+    // cleanup tasks are performed.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // This method should complete within approximately 5 seconds
+    // or the system may forcefully terminate the app
+    [self saveState];
 }
 
 @end
